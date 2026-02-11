e1c56cd5e84b2923e7bcd54d74751ac4e39036ed
LeBihanGael <gael.lebihan1606@gmail.com>
2026-02-11 16:50:59 +0100

    ajout js pour les rdv, modif css et html

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Js pour les popups de connexion et d'inscription (code non fait avec de l'ia mais avec des recherche sur internet)
 // Ouvrir les modals
 document.getElementById("btn-login").onclick = () => {
     document.getElementById("login-modal").style.display = "flex";
@@ -22,10 +23,7 @@ window.onclick = (e) => {
 };
 
 
-
-
-
-
+//Js pour la connexion
 const loginButton = document.getElementById("loginpop");
 
 loginButton.addEventListener("click", () => {
@@ -47,10 +45,13 @@ loginButton.addEventListener("click", () => {
     });
 });
 
+
+// Js pour l'inscription
 const boutonRegister = document.getElementById("registerpop");
 const myInput = document.getElementById("nameregis");
 const myInput2 = document.getElementById("passwordregis");
 const myInput3 = document.getElementById("emailregis");
+const myInput4 = document.getElementById("ageregis");
 
 boutonRegister.addEventListener("click", () => {
     fetch('/register', {
@@ -58,10 +59,33 @@ boutonRegister.addEventListener("click", () => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ login: myInput.value, password: myInput2.value, email: myInput3.value })
+        body: JSON.stringify({ login: myInput.value, password: myInput2.value, email: myInput3.value, age: myInput4.value })
     })
     .then(response => response.text())
     .then(data => {
         alert(data);
+    });
+});
+
+
+//Js pour la prise de rendez-vous
+const boutonRdv = document.getElementById("confirmrdv");
+const pseudoInput = document.getElementById("pseudo");
+const emailInput = document.getElementById("email");
+const dateInput = document.getElementById("date");
+const timeInput = document.getElementById("time");
+const lieuInput = document.getElementById("lieu");
+
+boutonRdv.addEventListener("click", () => {
+    fetch('/rdv', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ pseudoInput: pseudoInput.value, emailInput: emailInput.value, dateInput: dateInput.value, timeInput: timeInput.value, lieuInput: lieuInput.value })
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
     });
 });
