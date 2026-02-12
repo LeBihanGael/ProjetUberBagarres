0065a5703cc27123c131c5ec1da0bb2edee1cf02
tomatoos404 <thomas.touny991@gmail.com>
2026-02-12 09:39:41 +0100

    connection bdd

--- Public/server.js
+++ Public/server.js
@@ -2,3 +2,30 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql2');
 const ip = require("ip"); 
+
+app.listen(3000, () => {
+    let monIp = ip.address();
+    console.log(`Server lecture sur http://${monIp}:3000`);
+});
+
+
+const connection = mysql.createConnection({
+    host: '172.29.18.111',
+    user: 'jsServer',
+    password: 'jsServer',
+    database: 'ubercombat'
+});
+
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion à la BDD :', err);
+        return;
+    }
+    console.log('Connecté à la base de données MySQL.');
+});
+
+app.use(express.static('public')); 
+app.use(express.json());
+
+
