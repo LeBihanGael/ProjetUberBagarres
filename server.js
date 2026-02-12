8adac7e6da9f1c3a5bbe5b984feb5785b762640d
tomatoos404 <thomas.touny991@gmail.com>
2026-02-12 14:54:08 +0100

    route register

--- server.js
+++ server.js
@@ -28,4 +28,68 @@ connection.connect((err) => {
 app.use(express.static('Public')); 
 app.use(express.json());
 
+// ROUTE POUR RECUPERER LES USERS
 
+app.get('/users', (req, res) => {
+    connection.query('SELECT * FROM User', (err, results) => {
+        if (err) {
+            res.status(500).json({ message: 'Erreur serveur' });
+            return;
+        }
+        res.json(results);
+    });
+});
+
+
+
+// ROUTE POUR ENREEGISTRER UN NOUVEAU USER
+app.post('/register', (req, res) => {
+    const { login, email, password, age } = req.body;
+    connection.query(
+        'INSERT INTO utilisateur (pseudo, email, password, age) VALUES (?, ?)',
+        [ login,email, password ,age],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ message: 'Erreur serveur' });
+                return;
+            }
+            res.json({ message: 'Inscription réussie !', id_user: results.insertId });
+        }
+    );
+});
+
+
+// ROUTE POUR LA CONNEXION
+app.post('/connexion', (req, res) => {
+    const { login, password } = req.body;
+    connection.query('SELECT * FROM User WHERE login = ? AND password = ?', [login, password], (err, results) => {
+        if (err) {
+            res.status(500).json({ message: 'Erreur serveur' });
+            return;
+        }
+        if (results.length === 0) {
+            res.status(401).json({ message: 'Identifiants invalides' });
+            return;
+        }
+        res.json({ message: 'Connexion réussie !', User: results[0] });
+    });
+});
+
+
+
+// RECUPERER UN USER PAR SON ID
+app.post('/connexionId', (req, res) => {
+    const { userId } = req.body;
+    connection.query('SELECT id, login FROM User WHERE id = ?', [userId], (err, results) => {
+        if (err) {
+            res.status(500).json({ message: 'Erreur serveur' });
+            return;
+        }
+        if (results.length > 0) {
+            res.json(results[0]);
+        } else {
+            res.status(404).json({ message: 'Utilisateur introuvable' });
+        }
+    });
+});
